1f708168f229d8eeedca639fc8cd94cc979ca85b
CaioFML <caio.felipemotta@hotmail.com>
2020-10-24 00:43:02 -0300

    Adiciona scope within para store

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,4 +3,10 @@ class Store < ApplicationRecord
   validates :google_place_id, uniqueness: true, presence: true
 
   has_many :ratings
+
+  scope :within, -> (longitude, latitude, distance_in_km = 5) {
+    where(%{
+    ST_Distance(lonlat, 'POINT(%f %f)') < %d
+    } % [longitude, latitude, distance_in_km * 1000])
+  }
 end
